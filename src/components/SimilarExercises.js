1a4710002cd981d6a5403f59cd3d0e0ee040202b
jenny-hub90 <jumbledjenny19@gmail.com>
2023-05-01 21:01:59 +0545

    added similar videos of exercises from youtube

---
+++ src/components/SimilarExercises.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
+
+const SimilarExercises = () => {
+  return (
+    <div>SimilarExercises</div>
+  )
+}
+
+export default SimilarExercises
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,4 +1,5 @@
-const API_KEY = "e893e8c32cmsh248eaff5e87972fp132e8ejsnd755f8bb5e25"
+const API_KEY = "e893e8c32cmsh248eaff5e87972fp132e8ejsnd755f8bb5e25";
+// const API_KEY1 = "e893e8c32cmsh248eaff5e87972fp132e8ejsnd755f8bb5e25";
 export const exerciseOptions = {
         method: 'GET',
         headers: {
@@ -7,6 +8,15 @@ export const exerciseOptions = {
         }
       
 };
+
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key':API_KEY,
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+};
+
 export const fetchData = async(url, options)=>{
     const response = await fetch(url, options);
     const data = await response.json();
